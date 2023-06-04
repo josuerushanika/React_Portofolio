42919a91acbc16e187bcd95d4634d0f419019c77
josuerushanika <josuerushanika@gmail.com>
2023-06-04 17:54:09 +0200

    Add icon-for-sidebar

--- client/src/Components/Sidebar/Sidebar.js
+++ client/src/Components/Sidebar/Sidebar.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import "./Sidebar.css";
+import Home from '../Home/Home'
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
 const Sidebar = () => {
   return (
     <div className='container-fluid sidebar-section'>
        
+       <div className='sidebar'>
+
+          <div className='icon-for-sidebar-expand-and-collapse'>
+            <p> <BsChevronCompactLeft size={30}/> </p>
+          </div>
+
+       </div>
+
+       <div className='container'>
+            <Home/>
+       </div>
+
+
     </div>
   )
 }
