1beb51a6e0aa84843ab7f6b784de693bbd9bdf29
josuerushanika <josuerushanika@gmail.com>
2023-06-05 21:10:01 +0200

    Update tech star

--- client/src/Components/TechStack/TechStack.js
+++ client/src/Components/TechStack/TechStack.js
@@ -84,6 +84,31 @@ const TechStack = () => {
         },
     ]
 
+    const colors = [
+    "#F1C40F",
+    "#4B088A",
+    "#8181F7",
+    "#FE2EF7",
+    "#585858",
+    "#800000",
+    "#0088FE",
+    "#00C49F",
+    "#FFBB28",
+    "#FF8042",
+    "#001CCE",
+    "#00C79F",
+    "#FFBB24",
+    "#FF1042",
+    "#FF1042",
+    "#8181F7",
+    "#FE2EF7",
+    "#585858",
+    "#800000",
+    "#0088FE",
+    "#00C49F",
+
+    ];
+
   return (
     <div className='container techstack-section'>
        <div className='section-title'>
@@ -92,8 +117,11 @@ const TechStack = () => {
 
        <div className='row'>
            {data.map((item,index) =>(
-             <div className='col-xl-4 col-lg-4 col-md-6 col-sm-12 left-max'>
+             <div className='col-xl-4 col-lg-4 col-md-6 col-sm-12 left-max' key={index}>
              <div className='tech-content'>
+                <span className='tech-number' style={{backgroundColor: colors[index]}}>
+                    {index +1 }
+                </span>
                 <p>{item.name}</p>
                 </div>
              </div>
